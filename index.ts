c055549d5b6006e74b4471b52c0656b44f5c906b
Piotr Żaczek <zelowski@gmail.com>
2025-09-09 11:22:17 +0200

    add test for happy path for delta change

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
+import { BinanceAPI } from "./api/binance";
+import { calculateDeltaChange } from "./api/calculations";
+import { mapResponseToKlines } from "./api/mappers";
+
 console.log("Hello via Bun!");
+
+
+const app = async () => {
+
+	const binanceAPI = new BinanceAPI();
+	const klines = await binanceAPI.getKlines({ symbol: "BTCUSDT", interval: "1m", timeRange: { startTime: 1715328000000, endTime: 1715328000000 } });
+
+	const metrics = klines.map(mapResponseToKlines)
+	console.log('metrics', metrics)
+
+	const deltaChange = calculateDeltaChange(metrics)
+	console.log(deltaChange)
+
+}
+
+app();
